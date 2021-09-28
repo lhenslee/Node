ee9d577146b4f6f17587f6b025bfc162275ee989
Lane Henslee <lane.henslee@arcturus-uav.com>
2021-09-28 13:00:18 -0700

    done with part one

--- 4. website/server.js
+++ 4. website/server.js
@@ -1,13 +1,22 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
+console.log('restarting');
+
+app.use(express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req, res) => {
-    res.send('Hello Express :)');
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'speakers.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Express server listening on http://127.0.0.1:${port}`);
+  console.log(`Express server listening on http://127.0.0.1:${port}`);
 });
