d301833451de183d79ee3f458991ae391fb68982
Lane Henslee <lane.henslee@arcturus-uav.com>
2021-09-29 08:01:08 -0700

    prettier now avoids parenthesis

--- 4. website/routes/feedback.js
+++ 4. website/routes/feedback.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-module.exports = (params) => {
+module.exports = params => {
   const { feedbackService } = params;
 
   router.get("/", async (req, res) => {
