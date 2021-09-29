a49dd970a9954cf8176f9ccaa08d79835de8b4b8
lhenslee <lhenslee@vols.utk.edu>
2021-09-29 14:45:30 -0700

    making top speakers dynamic

--- 4. website/routes/index.js
+++ 4. website/routes/index.js
@@ -6,13 +6,11 @@ const feedbackRoute = require("./feedback");
 const router = express.Router();
 
 module.exports = params => {
-  router.get("/", (req, res) => {
-    if (!req.session.visitcount) {
-      req.session.visitcount = 0;
-    }
-    req.session.visitcount += 1;
+  const { speakerService } = params;
 
-    res.render("layout", { pageTitle: "Welcome", template: "index" });
+  router.get("/", async (req, res) => {
+    const topSpeakers = await speakerService.getList();
+    res.render("layout", { pageTitle: "Welcome", template: "index", topSpeakers });
   });
 
   router.use("/speakers", speakersRoute(params));
