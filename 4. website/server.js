e568f1fe1666f37726dc0804b32bd4bdcc40417c
lhenslee <lhenslee@vols.utk.edu>
2021-09-29 13:17:40 -0700

    starting ch4

--- 4. website/server.js
+++ 4. website/server.js
@@ -27,7 +27,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.static(path.join(__dirname, "static")));
-a;
+
 app.use("/", routes({ feedbackService, speakerService }));
 
 app.listen(port, () => {
