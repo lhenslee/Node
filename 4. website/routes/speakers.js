3fa1aa572c69d260b14e3514f2509990ba7e7c8e
Lane Henslee <lane.henslee@arcturus-uav.com>
2021-09-29 08:04:24 -0700

    just need to fix the SpeakerService.js

--- 4. website/routes/speakers.js
+++ 4. website/routes/speakers.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-module.exports = () => {
+module.exports = params => {
+  const { speakerService } = params;
 
   router.get("/", async (req, res) => {
-    res.json('Speakers!');
+    const speakers = speakerService.getList();
+    res.json(speakers);
   });
 
   router.get("/:shortname", (req, res) => {
--- 4. website/server.js
+++ 4. website/server.js
@@ -3,8 +3,10 @@ const path = require("path");
 const routes = require("./routes");
 
 const FeedbackService = require("./services/FeedbackService");
+const SpeakerService = require("./services/SpeakerService");
 
 const feedbackService = new FeedbackService("./data/feedback.json");
+const speakerService = new SpeakerService("./data/speakers.json");
 
 const app = express();
 
@@ -15,7 +17,7 @@ app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.static(path.join(__dirname, "static")));
 
-app.use("/", routes({ feedbackService }));
+app.use("/", routes({ feedbackService, speakerService }));
 
 app.listen(port, () => {
   console.log(`Express server listening on http://127.0.0.1:${port}`);
