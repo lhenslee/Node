7e9e81018279e10535acb6a0dac1895ea309229c
lhenslee <lhenslee@vols.utk.edu>
2021-09-30 15:44:26 -0700

    input is sanitized

--- 4. website/routes/feedback.js
+++ 4. website/routes/feedback.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
 
@@ -8,17 +9,47 @@ module.exports = params => {
   router.get("/", async (req, res, next) => {
     try {
       const feedback = await feedbackService.getList();
-      return res.render("layout", { pageTitle: "Feedback", template: "feedback", feedback });
+
+      const errors = req.session.feedback ? req.session.feedback.errors : false;
+      req.session.feedback = {};
+
+      return res.render("layout", {
+        pageTitle: "Feedback",
+        template: "feedback",
+        feedback,
+        errors,
+      });
     } catch (err) {
       return next(err);
     }
   });
 
-  router.post("/", async (req, res, next) => {
-    const feedback = await feedbackService.getList();
-    feedback.push(req.body);
-    res.render("layout", { pageTitle: "Feedback Submitted!", template: "feedback", feedback });
-  });
+  router.post(
+    "/",
+    [
+      check("name").trim().isLength({ min: 3, max: 30 }).escape().withMessage("A name is required"),
+      check("email").trim().isEmail().normalizeEmail().withMessage("A normal email is required"),
+      check("title")
+        .trim()
+        .isLength({ min: 3, max: 40 })
+        .escape()
+        .withMessage("A title is required"),
+      check("message")
+        .trim()
+        .isLength({ min: 5, max: 255 })
+        .escape()
+        .withMessage("A message is required"),
+    ],
+    async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        req.session.feedback = {
+          errors: errors.array(),
+        };
+        return res.redirect("/feedback");
+      }
+    }
+  );
 
   return router;
 };
